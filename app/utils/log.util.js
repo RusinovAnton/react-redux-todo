404e9d575877e6d0c0b010c43454f4a4baf3136d
Rusinov.anton <rusinov.amgrade@gmail.com>
2016-07-08 11:07:59 +0300

    add stack tracing for log utility

--- app/utils/log.util.js
+++ app/utils/log.util.js
@@ -8,17 +8,24 @@ export default class Log {
         this.version = '1.0.0'
     }
 
-    static reduce(str) {
+    static getStack() {
+        console.log(`%c${new Error().stack.replace(/Error/, 'Stack trace')}`, 'font-size:11px');
+    }
+
+    static reduce(str, getStack = false) {
+        if (getStack) Log.getStack();
         console.log(`%c${str}`, 'color: blue; font-weight: bold');
         console.log(...logSeparator);
     }
 
-    static action(str) {
+    static action(str, getStack = false) {
+        if (getStack) Log.getStack();
         console.log(`%c${str}`, 'color: red; font-weight: bold');
         console.log(...logSeparator);
     }
 
-    static render(str) {
+    static render(str, getStack = false) {
+        if (getStack) Log.getStack();
         console.log(`%c${str}`, 'color: green; font-weight: bold');
         console.log(...logSeparator);
     }
